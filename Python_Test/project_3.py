ec628765d0c8151ab4aab277cd043c54a44e32e7
SSC202 <e22750706642022@163.com>
2023-11-28 21:21:33 +0800

    增加蓝色球阈值判断

--- Python_Test/project_3.py
+++ Python_Test/project_3.py
@@ -52,6 +52,8 @@ lower_purple = np.array([137, 143, 0])
 upper_purple = np.array([179, 203, 255])
 lower_red = np.array([117, 143, 0])
 upper_red = np.array([133, 255, 255])
+lower_blue = np.array([0, 159, 0])
+upper_blue = np.array([16, 255, 255])
 # 3. 结构元素定义
 kernel = np.ones((3, 3), np.uint8)
 
@@ -96,6 +98,8 @@ try:
         red_mask = cv2.inRange(blendSRaisen, lower_red, upper_red)
         red_img = cv2.bitwise_and(color_image, color_image, mask=red_mask)
         # cv2.imshow('result',red_img)
+        blue_mask = cv2.inRange(blendSRaisen, lower_blue, upper_blue)
+        blue_img = cv2.bitwise_and(color_image, color_image, mask=blue_mask)
         """
             3. 滤波
         """
@@ -103,11 +107,13 @@ try:
         # red_img = cv2.GaussianBlur(red_img, (3, 3), 0)
         purple_img = cv2.medianBlur(purple_img, 5)
         red_img = cv2.medianBlur(red_img, 5)
+        blue_img = cv2.medianBlur(blue_img, 5)
         """
             4. 二值化
         """
         purple_gray = cv2.cvtColor(purple_img, cv2.COLOR_RGB2GRAY)
         red_gray = cv2.cvtColor(red_img, cv2.COLOR_BGR2GRAY)
+        blue_gray = cv2.cvtColor(blue_img, cv2.COLOR_BGR2GRAY)
         # purple_gray = cv2.GaussianBlur(purple_gray, (3, 3), 0)
         # red_gray = cv2.GaussianBlur(red_gray, (3, 3), 0)
         # cv2.imshow('result',purple_gray)
@@ -117,13 +123,17 @@ try:
             purple_gray, 0, 255, cv2.THRESH_BINARY)
         res, red_thre = cv2.threshold(
             red_gray, 0, 255, cv2.THRESH_BINARY)
+        res, blue_thre = cv2.threshold(
+            blue_gray, 0, 255, cv2.THRESH_BINARY)
         # cv2.imshow('result', purple_thre)
         # cv2.imshow('result', red_thre)
 
         purple_thre = cv2.morphologyEx(purple_thre, cv2.MORPH_OPEN, kernel)
         red_thre = cv2.morphologyEx(red_thre, cv2.MORPH_OPEN, kernel)
+        blue_thre = cv2.morphologyEx(blue_thre, cv2.MORPH_OPEN, kernel)
         purple_thre = cv2.morphologyEx(purple_thre, cv2.MORPH_CLOSE, kernel)
         red_thre = cv2.morphologyEx(red_thre, cv2.MORPH_CLOSE, kernel)
+        blue_thre = cv2.morphologyEx(blue_thre, cv2.MORPH_CLOSE, kernel)
         cv2.imshow('result_thre', red_thre)
         """
             5. 球的检测
@@ -180,6 +190,109 @@ try:
                         cv2.circle(color_image, center_red,
                                    radius_red, (0, 0, 255), 3)
 
+        purple_circles = cv2.HoughCircles(purple_thre, cv2.HOUGH_GRADIENT_ALT,
+                                       1.5, 20, param1=30, param2=0.50, minRadius=10, maxRadius=200)
+        # 如果找到了球，则直接判定
+        if purple_circles is not None:
+            filter_circle = []
+            purple_circles = np.uint16(np.around(purple_circles))
+            for cir_ in purple_circles:
+                cir = cir_[0]
+                # 凹凸判断，通过创建掩膜进行逻辑与进行凹凸性判断
+                x = cir[0]
+                y = cir[1]
+                r = cir[2]
+                purple_circle_mask = np.zeros_like(purple_thre)
+                cv2.circle(purple_circle_mask, (x, y), r, 255, thickness=-1)
+                pixel_count = np.sum(np.logical_and(
+                    purple_circle_mask, purple_thre) > 0)
+                if pixel_count > 0.9 * np.pi * r * r:
+                    filter_circle.append((x, y, r))
+            if len(filter_circle) > 0:
+                print(filter_circle)
+                for cir_ in filter_circle:
+                    cv2.circle(
+                        color_image, (cir_[0], cir_[1]), cir_[2], (0, 255, 255), 2)
+                    cv2.circle(color_image, (cir_[0], cir_[1]), 2, (255, 255, 0), 2)
+            else:
+                # 提取轮廓
+                contours_purple, hierarchy_purple = cv2.findContours(
+                    purple_thre, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                cv2.drawContours(
+                    color_image, contours_purple, -1, (0, 255, 255), 1)
+                if not contours_purple:
+                    pass
+                else:
+                    # 寻找最大面积的轮廓
+                    areas_purple = []
+                    for c in range(len(contours_purple)):
+                        areas_purple.append(cv2.contourArea(contours_purple[c]))
+
+                    max_id_purple = areas_purple.index(max(areas_purple))
+                    print(max_id_purple)
+                    # 椭圆拟合
+                    if contours_purple[max_id_purple].size < 50:
+                        pass
+                    else:
+                        (x_purple, y_purple), radius_purple = cv2.minEnclosingCircle(
+                            contours_purple[max_id_purple])
+                        center_purple = (int(x_purple), int(y_purple))
+                        radius_purple = int(radius_purple)
+                        cv2.circle(color_image, center_purple,
+                                   radius_purple, (0, 0, 255), 3)
+                        
+        # 先进行霍夫圆变换
+        blue_circles = cv2.HoughCircles(blue_thre, cv2.HOUGH_GRADIENT_ALT,
+                                       1.5, 20, param1=30, param2=0.50, minRadius=10, maxRadius=200)
+        # 如果找到了球，则直接判定
+        if blue_circles is not None:
+            filter_circle = []
+            blue_circles = np.uint16(np.around(blue_circles))
+            for cir_ in blue_circles:
+                cir = cir_[0]
+                # 凹凸判断，通过创建掩膜进行逻辑与进行凹凸性判断
+                x = cir[0]
+                y = cir[1]
+                r = cir[2]
+                blue_circle_mask = np.zeros_like(blue_thre)
+                cv2.circle(blue_circle_mask, (x, y), r, 255, thickness=-1)
+                pixel_count = np.sum(np.logical_and(
+                    blue_circle_mask, blue_thre) > 0)
+                if pixel_count > 0.9 * np.pi * r * r:
+                    filter_circle.append((x, y, r))
+            if len(filter_circle) > 0:
+                print(filter_circle)
+                for cir_ in filter_circle:
+                    cv2.circle(
+                        color_image, (cir_[0], cir_[1]), cir_[2], (0, 255, 255), 2)
+                    cv2.circle(color_image, (cir_[0], cir_[1]), 2, (255, 255, 0), 2)
+            else:
+                # 提取轮廓
+                contours_blue, hierarchy_blue = cv2.findContours(
+                    blue_thre, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                cv2.drawContours(
+                    color_image, contours_blue, -1, (0, 255, 255), 1)
+                if not contours_blue:
+                    pass
+                else:
+                    # 寻找最大面积的轮廓
+                    areas_blue = []
+                    for c in range(len(contours_blue)):
+                        areas_blue.append(cv2.contourArea(contours_blue[c]))
+
+                    max_id_blue = areas_blue.index(max(areas_blue))
+                    print(max_id_blue)
+                    # 椭圆拟合
+                    if contours_blue[max_id_blue].size < 50:
+                        pass
+                    else:
+                        (x_blue, y_blue), radius_blue = cv2.minEnclosingCircle(
+                            contours_blue[max_id_blue])
+                        center_blue = (int(x_blue), int(y_blue))
+                        radius_blue = int(radius_blue)
+                        cv2.circle(color_image, center_blue,
+                                   radius_blue, (0, 0, 255), 3)
+                        
         cv2.imshow('result', color_image)
         print("--------------")
 
